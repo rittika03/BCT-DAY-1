116bce169bb1f9850914e9f1e94975678dc812d5
rittika03 <rittikasamanta03@gmail.com>
2025-02-28 21:55:37 +0530

    Delete Q2-Leap Year.py

--- Q2-Leap Year.py
+++
@@ -1,7 +0,0 @@
-year=int(input("Enter a Year"))
-if(year%4 == 0 & year%100 != 0):
-    print(year," is a Leap Year.")
-elif(year%100 != 0 & year%400 == 0):
-    print(year," is a Leap Year.")
-else:
-    print(year,"is not a Leap Year.")
