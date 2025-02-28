5b838a74ac7224ff8678fbe24d826bad8c6e95a8
rittika03 <rittikasamanta03@gmail.com>
2025-02-28 21:59:03 +0530

    Delete Q9-Swapping Numbers.py

--- Q9-Swapping Numbers.py
+++
@@ -1,7 +0,0 @@
-a=int(input("Enter the value of a:"))
-b=int(input("Enter the value of b:"))
-print("Value of a before swapping= ",a)
-print("Value of b before swapping= ",b)
-a,b=b,a
-print("Value of a after swapping= ",a)
-print("Value of b after swapping= ",b)
